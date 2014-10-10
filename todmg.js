0b8610d57da14a1f5feb1a32f9b16de6c786adf3
Lucas Vieira <vieiralucas4@gmail.com>
2014-10-10 11:22:29 -0300

    add champion stuff

---
+++ todmg.js
@@ -0,0 +1,50 @@
+var https = require('https');
+
+function todmg(key) {
+	this.key = key;
+}
+
+todmg.prototype.getChampionsStatus = function(region, callback) {
+	var url         = 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.2/champion?api_key=' + this.key,
+		champions   = "";
+
+	https.get(url, function(res) {
+		res.on('error', function(err) {
+			callback(err);
+		});
+		res.on('data', function(data) {
+			champions += data.toString();
+		});
+		res.on('end', function() {
+			callback(null, JSON.parse(champions).champions);
+		});
+	});
+	
+};
+
+todmg.prototype.getChampionsNames = function(region, callback) {
+	var url       = 'https://' + region + '.api.pvp.net/api/lol/static-data/' + region + '/v1.2/champion?api_key=' + this.key,
+		champions = "";
+
+		https.get(url, function(res) {
+			res.on('error', function(err) {
+				callback(err);
+			});
+			res.on('data', function(data) {
+				champions += data.toString();
+			});
+			res.on('end', function() {
+				var data    = JSON.parse(champions).data,
+					response = [];
+				for(var d in data) {
+					if(data.hasOwnProperty(d)) {
+						response.push({id: data[d].id, key: data[d].key, name: data[d].name, title: data[d].title});
+					} 
+				}
+				callback(null, response);
+			})
+		});
+};
+
+
+module.exports = todmg;
