6a3758247dbebbee4c88eca111a332e06b1cccbb
Lucas Vieira <vieiralucas4@gmail.com>
2014-10-11 02:36:39 -0300

    add summoner stuff

--- tonsofdamage.js
+++ tonsofdamage.js
@@ -3,12 +3,67 @@
 
     function TonsOfDamage(key) {
         this.key = key;
-        this.champions = {}; // the idea is {na: {champs, idSearch} br: {champs, idSearch, nameSearch}}
+        this.champions = {}; // the idea is {na: {champs, idSearch, nameSearch}, br: {champs, idSearch, nameSearch}}
     }
 
     TonsOfDamage.create = function(key) {
         return new TonsOfDamage(key);
     }
+
+    TonsOfDamage.prototype.getChampionsInfo = function(region, callback) {
+        if(this.champions[region]) {
+            return callback(null, this.champions[region].champs);
+        }
+        var status = null, 
+            names  = null,
+            result = [],
+            that   = this;
+
+        this.getChampionsStatus(region, function(err, champs) {
+            if(err) {
+                return callback(err);
+            }
+            status = champs;
+            if(names) {
+                join();
+            }
+        });
+
+        this.getChampionsName(region, function(err, champs) {
+            if(err) {
+                return callback(err);
+            }
+            names = champs;
+            if(status) {
+                join();
+            }
+        });
+
+        function join() {
+            for(var s = 0; s < status.length; s++) {
+                for(var n = 0; n < names.length; n++) {
+                    if(status[s].id === names[n].id) {
+                        result.push(mergeObjects(status[s], names[n]));
+                        names.splice(n, 1);
+                    }
+                }
+            }
+            result.sort(function(a, b) {
+                return Number(a.id) - Number(b.id);
+            })
+            that.champions[region] = {};
+            that.champions[region].champs = result;
+            var idSearch   = {},
+                nameSearch = {};
+            for (var i = 0; i < result.length; i++) {
+                idSearch[result[i].id] = result[i];
+                nameSearch[result[i].name.toLowerCase()] = result[i];
+            }
+            that.champions[region].idSearch = idSearch;
+            that.champions[region].nameSearch = nameSearch;
+            callback(null, result);
+        }
+    };
 
     TonsOfDamage.prototype.getChampionsStatus = function(region, callback) {
         var url       = 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.2/champion?api_key=' + this.key,
@@ -78,62 +133,72 @@
         })  
     };
 
-    TonsOfDamage.prototype.getChampionsInfo = function(region, callback) {
-        if(this.champions[region]) {
-            return callback(null, this.champions[region].champs);
+    TonsOfDamage.prototype.getSummonerInfo = function(region, name, callback) {
+        var url      = 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.4/summoner/by-name/' + name + '?api_key=' + this.key,
+            summoner = "";
+        https.get(url, function(res) {
+            res.on('error', function(err) {
+                return callback(url);
+            });
+            res.on('data', function(data) {
+                summoner += data.toString();
+            });
+            res.on('end', function() {
+                callback(null, JSON.parse(summoner)[name.replace(' ', '')]);
+            })
+        });
+    };
+
+    TonsOfDamage.prototype.getSummonerStats = function(region, name, season, callback) {
+        var that = this;
+        this.getSummonerInfo(region, name, function(err, summoner) {
+            var id  = summoner.id,
+                url = 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + id + '/ranked?season=SEASON' + season + '&api_key=' + that.key;
+            https.get(url, function(res) {
+                var stats = "";
+                res.on('error', function(err) {
+                    return callback(err);
+                });
+                res.on('data', function(data) {
+                    stats += data.toString();
+                });
+                res.on('end', function() {
+                    callback(null, JSON.parse(stats)['champions']);
+                });
+            });
+        })
+    };
+
+    TonsOfDamage.prototype.getSummonerStatsByChampionName = function(region, name, season, champion, callback) {
+        var that = this;
+        if(!this.champions[region]) {
+            this.getChampionsInfo(region, function(err, champ) {
+                if(err) {
+                    return callback(err);
+                }
+                proceed();
+            });
+        } else {
+            proceed();
         }
-        var status = null, 
-            names  = null,
-            result = [],
-            that   = this;
 
-        this.getChampionsStatus(region, function(err, champs) {
-            if(err) {
-                return callback(err);
-            }
-            status = champs;
-            if(names) {
-                join();
-            }
-        });
-
-        this.getChampionsName(region, function(err, champs) {
-            if(err) {
-                return callback(err);
-            }
-            names = champs;
-            if(status) {
-                join();
-            }
-        });
-
-        function join() {
-            for(var s = 0; s < status.length; s++) {
-                for(var n = 0; n < names.length; n++) {
-                    if(status[s].id === names[n].id) {
-                        result.push(mergeObjects(status[s], names[n]));
-                        names.splice(n, 1);
+        function proceed() {
+            var champId = that.champions[region].nameSearch[champion].id;
+            that.getSummonerStats(region, name, season, function(err, stats) {
+                if(err) {
+                    return callback(err);
+                }
+                for(var i = 0; i < stats.length; i++) {
+                    if(stats[i].id === champId) {
+                        return callback(null, stats[i].stats);
                     }
                 }
-            }
-            result.sort(function(a, b) {
-                return Number(a.id) - Number(b.id);
+                callback(null, null);
             })
-            that.champions[region] = {};
-            that.champions[region].champs = result;
-            var idSearch   = {},
-                nameSearch = {};
-            for (var i = 0; i < result.length; i++) {
-                idSearch[result[i].id] = result[i];
-                nameSearch[result[i].name.toLowerCase()] = result[i];
-            }
-            that.champions[region].idSearch = idSearch;
-            that.champions[region].nameSearch = nameSearch;
-            callback(null, result);
         }
     };
 
-    function mergeObjects(obj1, obj2){
+    function mergeObjects(obj1, obj2) {
         var obj3 = {};
         for (var attr in obj1) { 
             obj3[attr] = obj1[attr];
@@ -141,8 +206,8 @@
         for (var attr in     obj2) {
             obj3[attr] = obj2[attr];
         }
-            return obj3;
-        }
+        return obj3;
+    }
 
     module.exports = TonsOfDamage;
 }());
